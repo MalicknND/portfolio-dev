d3dfd3d331507caed34860f08dcc15db6418cf81
MalicknND <ndiayemalick062@gmail.com>
2024-06-03 18:52:21 +0200

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,19 @@
+export const personalData = {
+  name: "Malick Siguy NDIAYE",
+  profile: "/profile.jpeg",
+  designation: "Full Stack Developer",
+  description:
+    "My name is Malick Siguy. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "ndiayemalicksiguy@gmail.com",
+  phone: "+33 7 66 73 12 63",
+  address: "Paris, France",
+  github: "https://github.com/said7388",
+  facebook: "https://www.facebook.com/abusaid.riyaz/",
+  linkedIn: "https://www.linkedin.com/in/abu-said-bd/",
+  twitter: "https://twitter.com/said7388",
+  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "malicknnd",
+  resume:
+    "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+};
